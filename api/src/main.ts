c3a270e10327e6b1fea15527d9b2911d7528a38f
alicesiqueira <aliicesiqueiraaa@gmail.com>
2024-03-03 21:32:09 -0300

    editing cors config

--- api/src/main.ts
+++ api/src/main.ts
@@ -3,8 +3,18 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+
+    allowedHeaders: ['Content-Type', 'Origin', 'X-Requested-With', 'Accept', 'Authorization'],
+
+    exposedHeaders: ['Authorization'],
+    credentials: true, 
+  
+  })
 
   await app.listen(3000);
 }
